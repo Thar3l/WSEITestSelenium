e0075afc7ad164a6af033375dddc87b1c8d4d3d0
Tharel <tharel.biznes@gmail.com>
2020-05-07 14:36:46 +0200

    init

---
+++ SeleniumTestWSEI/FacebookLoginFormTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestWSEI
+{
+
+    [TestFixture]
+    public class FacebookLoginFormTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:/Users/stere/Desktop/seleniumtest/ewlo");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Navigate().GoToUrl("http://www.facebook.com");
+            driver.FindElement(By.Id("email")).SendKeys("emailid");
+            driver.FindElement(By.Id("pass")).SendKeys("password");
+            driver.FindElement(By.Id("u_0_l")).Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ SeleniumTestWSEI/StepStoneFormTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTestWSEI
+{
+
+    [TestFixture]
+    public class StepStoneFormTest
+    {
+        IWebDriver driver;
+        string testFormString;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:/Users/stere/Desktop/seleniumtest/ewlo");
+
+            for (int i = 0; i < 100; i++)
+                testFormString += "@@@@@@@@@@";
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Navigate().GoToUrl("https://www.stepstone.pl/");
+            driver.FindElement(By.Name("ke")).SendKeys(testFormString);
+            driver.FindElement(By.Name("ws")).SendKeys("31-001");
+            driver.FindElement(By.Name("wt")).Click();
+            driver.FindElement(By.ClassName("search-box__submit")).Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
